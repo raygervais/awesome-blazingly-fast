c750bb316d589f5d657d1970d8aba585bcf47445
RayGervais <raygervais@hotmail.ca>
2022-05-23 15:36:03 -0400

    updates: list to use sorting based on languages

--- datagen.py
+++ datagen.py
@@ -51,6 +51,22 @@ def get_and_cache_repositories(get_repositories_paginated, total_pages, last_pag
             print(f"Page {page} already exists")
 
 
+def create_language_map(repos):
+    language_map = {"Other": []}
+
+    for repo in repos:
+        if repo["language"] is not None:
+            if repo["language"] in language_map:
+                language_map[repo["language"]].append(repo)
+            else:
+                language_map[repo["language"]] = [repo]
+        else:
+            print(f"{repo['full_name']} has no language setting, to other")
+            language_map["Other"].append(repo)
+
+    return language_map
+
+
 if __name__ == "__main__":
     # Determine initial amount of repositories
     repos = get_repositories("blazingly fast")
@@ -73,19 +89,39 @@ if __name__ == "__main__":
     get_and_cache_repositories(get_repositories_paginated, total_pages, last_page)
 
     # Parse the data files
-    repos = []
+    languages = {}
+
     for page in range(1, total_pages):
         with open(f"data/repositories-{page}.json") as f:
-            repos += json.load(f)
+            # create a hashmap of languages to repositories
+            page_languages = create_language_map(json.load(f))
+            # add the page's languages to the total
+            for language in page_languages:
+                if language in languages:
+                    languages[language] += page_languages[language]
+                else:
+                    languages[language] = page_languages[language]
 
-    print(f"Cached {len(repos)} repositories")
+    print(f"Cached {len(languages)} languages")
 
-    # Sort the repositories by stars
-    repos.sort(key=lambda r: r["stargazers_count"], reverse=True)
+    # sort the hashmap alphabetically
+    languages = {k: v for k, v in sorted(languages.items(), key=lambda item: item[0])}
 
-    # Print the first 10 repositories
+    # Save the language map to a file
     with open("README.md", "a") as f:
-        for repo in repos:
-            f.write(
-                f"- [{repo['name']}]({repo['html_url']}) - {repo['description']} - ⭐ {repo['stargazers_count']}\n"
-            )
+
+        # Write the table of contents
+        f.write("## Table of contents\n")
+        for language in languages:
+            f.write(f"- [{language}](#{language})\n")
+
+        f.write("\n")
+
+        # Write the header
+        for language, repositories in languages.items():
+            f.write(f"## {language}\n")
+            for repo in repositories:
+                f.write(
+                    f"- [{repo['name']}]({repo['html_url']}) - {repo['description']} - ⭐ {repo['stargazers_count']}\n"
+                )
+            f.write("\n")
